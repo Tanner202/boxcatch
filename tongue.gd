5b5496e1aed29d51781e306f4af6e630ab38a66f
Tanner <tannerelephant@gmail.com>
2024-11-02 10:59:43 -0700

    prevent player from spam catching

--- tongue.gd
+++ tongue.gd
@@ -4,20 +4,25 @@ extends Node2D
 @onready var timer: Timer = $Timer
 
 var is_catching = false
+var is_retracting = false
 var max_scale = 4
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("catch"):
+	if event.is_action_pressed("catch") and !is_catching:
 		is_catching = true
 	
 func _process(delta: float) -> void:
 	if is_catching:
-		if scale.y < max_scale:
-			scale.y += speed * delta
-		elif scale.y >= max_scale:
-			is_catching = false
-	else:
-		if scale.y > 0.01:
-			scale.y -= speed * delta
-		elif scale.y < 0:
-			scale.y = 0
+		if !is_retracting:
+			if scale.y < max_scale:
+				scale.y += speed * delta
+			elif scale.y >= max_scale:
+				is_retracting = true
+		else:
+			if scale.y > 0.01:
+				scale.y -= speed * delta
+			elif scale.y < 0.01:
+				scale.y = 0
+				is_catching = false
+				is_retracting = false
+				
