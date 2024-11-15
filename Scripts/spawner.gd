df8a6b0d6550938f4f51a2a24ac0f691cda13a01
Tanner <tannerelephant@gmail.com>
2024-11-15 13:55:29 -0800

    make spawner more generic and add fly spawner

---
+++ Scripts/spawner.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@export var item_to_spawn : PackedScene
+@export var is_horizontal = true
+@export var timer_cooldown = 1
+@export_range(0, 100, 1) var y_offset
+var screen_size
+@onready var timer: Timer = $Timer
+
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	timer.wait_time = timer_cooldown
+
+func _on_timer_timeout() -> void:
+	if is_horizontal:
+		spawn_horizontally()
+	else:
+		spawn_vertically()
+
+func spawn_horizontally():
+	var spawned_item = item_to_spawn.instantiate()
+	spawned_item.global_position = Vector2(position.x + randf_range(-screen_size.x/2, screen_size.x/2), global_position.y)
+	get_tree().root.get_node("Level").add_child(spawned_item)
+	
+func spawn_vertically():
+	var spawned_item = item_to_spawn.instantiate()
+	spawned_item.global_position = Vector2(global_position.x, position.y + randf_range(-y_offset, y_offset))
+	get_tree().root.get_node("Level").add_child(spawned_item)
