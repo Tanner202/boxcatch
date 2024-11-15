3e4a742b01475b6611e0f024d5848367b5360047
Tanner <tannerelephant@gmail.com>
2024-11-14 18:43:48 -0800

    add basic settings menu

---
+++ Scripts/settings_menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+func _on_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(0, value)
+
+
+func _on_check_box_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(0, toggled_on)
+
+
+func _on_fullscreen_check_box_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+
+func _on_exit_button_pressed() -> void:
+	print("exit")
