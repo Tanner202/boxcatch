ee237d51cc4a7edacb7fe0f476aa56fda88a7664
Tanner <tannerelephant@gmail.com>
2024-11-17 10:43:09 -0700

    add basic button remapping

--- Scripts/settings_menu.gd
+++ Scripts/settings_menu.gd
@@ -1,5 +1,41 @@
 extends Control
 
+@onready var input_button = preload("res://Scenes/input_button.tscn")
+@onready var v_box_container: VBoxContainer = $Panel/MarginContainer/ScrollContainer/VBoxContainer
+
+var is_rebinding = false
+var active_button : Button
+var action_to_rebind : String
+
+var inputs = {"left": "Left", "right": "Right", "slow_down": "Slow Down", "catch": "Catch"}
+
+func _ready() -> void:
+	create_input_settings()
+	
+func create_input_settings():
+	for input in InputMap.get_actions():
+		if input in inputs:
+			var button = input_button.instantiate()
+			v_box_container.add_child(button)
+			button.find_child("ActionLabel").text = input
+			var input_text = InputMap.action_get_events(input)[0].as_text()
+			button.find_child("KeybindLabel").text = input_text.trim_suffix(" (Physical)")
+			button.pressed.connect(_on_input_button_pressed.bind(button, input))
+
+func _on_input_button_pressed(button, action_to_rebind):
+	is_rebinding = true
+	active_button = button
+	self.action_to_rebind = action_to_rebind
+
+func _input(event: InputEvent) -> void:
+	if is_rebinding and event is InputEventKey and event.is_pressed():
+		print("Rebinding to " + str(event.pressed))
+		InputMap.action_erase_events(action_to_rebind)
+		InputMap.action_add_event(action_to_rebind, event)
+		active_button.find_child("KeybindLabel").text = event.as_text()
+		is_rebinding = false
+		accept_event()
+
 
 func _on_h_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(0, value)
